9cfc2f42eec5156fa844e5948b2f7910cd66dfac
pulkitrathi17 <pulkitrathi98@gmail.com>
2017-08-23 16:16:37 +0530

    login

--- app/src/main/java/com/example/soumyaagarwal/customerapp/CustomerApp.java
+++ app/src/main/java/com/example/soumyaagarwal/customerapp/CustomerApp.java
@@ -18,7 +18,7 @@ public class CustomerApp extends android.support.multidex.MultiDexApplication {
     public static DatabaseReference DBREF;
     private CustomerSession session;
     public static SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy hh:mm aa");
-    public static String AppName = "MeChat";
+    public static String AppName = "demo";
 
     @Override
     public void onCreate() {
